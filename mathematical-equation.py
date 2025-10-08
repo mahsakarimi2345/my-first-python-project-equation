b8c5d97072d97f17cf8c73009ff6c2bc0df76067
Mahsa Karimi <mahsakarimi2345@gmail.com>
2025-10-08 16:26:36 +0330

    Update mathematical-equation.py

--- mathematical-equation.py
+++ mathematical-equation.py
@@ -1,10 +1,10 @@
 #Math game: every number leads to 8.
 
-first_number = input('choose a number between 1 - 9 :')   #ask the user to choose a number bet1ween 1 - 9
+first_number = input('choose a number between 1 - 9 :')   #ask the user to choose a number bet1ween 1 - 9.
 
-first_number = int(first_number)     #input() returns a string, so we use int() to convert it to an integer
+first_number = int(first_number)     #input() returns a string, so we use int() to convert it to an integer.
 
-number = first_number   #Assign number to first_number for calculations 
+number = first_number   #Assign number to first_number for calculations.
 number *= 2
 number += 8
 number += first_number
@@ -19,5 +19,6 @@ print(number)
 #   💻 Code by Mahsa ✨
 #   📅 2025
 # ──────────────────────────────
+
 
 
